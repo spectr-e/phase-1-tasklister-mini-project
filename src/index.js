b0925a21b8d7b7b2f3bd4c4ee94aa6a5ee14a6fa
josiah <devs.josia@gmail.com>
2022-12-03 11:56:47 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    showToDo(e.target.new_task_description.value)
+  })
 });
+function showToDo(task){
+  let pTasks = document.createElement('p')
+  let pDelBtn = document.createElement('button')
+  pDelBtn.textContent = 'x'
+  pDelBtn.style.marginLeft = '12px'
+  pDelBtn.addEventListener('click', () => {
+    pTasks.remove()
+  })
+  pTasks.textContent = task
+  pTasks.appendChild(pDelBtn)
+  document.querySelector('#tasks').appendChild(pTasks)
+}
